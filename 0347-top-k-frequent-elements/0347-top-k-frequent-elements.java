e920dfce484d00e41eeb7be1181b8c2000cd1aff
Divyansh kackker <kackkerdivyansh1115@gmail.com>
2026-05-07 15:36:01 +0530

    Time: 14 ms (71.06%), Space: 47.2 MB (97.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,38 +1,43 @@
 class Solution {
-    class Pair {
-        int num;
-        int freq;
-
-        Pair(int num, int freq){
-            this.num = num;
-            this.freq = freq;
-        }
-    }
-
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent(int[] arr, int k) {
         int[] ans = new int[k];
-
+        int n=arr.length;
         Map<Integer, Integer> mp = new HashMap<>();
 
-        for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        for(int i = 0; i < arr.length; i++){
+            mp.put(arr[i], mp.getOrDefault(arr[i], 0) + 1);
         }
-
-        PriorityQueue<Pair> pq=new PriorityQueue<>(
-            (a,b)->{
-               return b.freq-a.freq;
-            }
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+                (a, b) -> a.freq - b.freq
         );
-
         for(int key: mp.keySet()){
             pq.add(new Pair(key,mp.get(key)));
-        }  
 
+            if(pq.size()>k){
+                pq.poll();
+            }
+
+        }
         for(int i=0;i<k;i++){
             ans[i]=pq.poll().num;
         }
 
 return ans;
-       
+    }
+
+
+    
+
+
+        
+    
+    class Pair{
+        int num;
+        int freq;
+
+        Pair(int num,int freq){
+            this.num=num;
+            this.freq=freq;
+        }
     }
 }
