124cb534a15dedb5f8ef90852c444e45ee88abb8
Divyansh kackker <kackkerdivyansh1115@gmail.com>
2026-05-09 02:00:32 +0530

    Time: 29 ms (83.22%), Space: 47.9 MB (59.47%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        Arrays.sort(hand);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int card : hand) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        for (int card : hand) {
+
+            
+            if (map.get(card) == 0) {
+                continue;
+            }
+
+            
+            for (int i = 0; i < groupSize; i++) {
+                int current = card + i;
+
+                if (!map.containsKey(current) || map.get(current) == 0) {
+                    return false;
+                }
+
+                map.put(current, map.get(current) - 1);
+            }
+        }
+
+        return true;
+    }
+}
