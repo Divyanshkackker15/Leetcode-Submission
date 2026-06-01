d45180cc09796c1eb0e1215be6c4bcb514fe9f73
Divyansh kackker <kackkerdivyansh1115@gmail.com>
2026-06-01 13:47:15 +0530

    Time: 1 ms (98.29%), Space: 47.4 MB (79.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,64 @@
 class Solution {
-    public void setZeroes(int[][] arr) {
-        int n=arr.length;
-        int m=arr[0].length;
-       int[]row=new int[n];
-       int [] col=new int [m];
 
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(arr[i][j]==0)
-            {
-                row[i]=1;
-                col[j]=1;
+    public void setZeroes(int[][] arr) {
+
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int col0 = 1;
+
+        // Step 1: Mark rows and columns
+        for(int i = 0; i < n; i++) {
+
+            for(int j = 0; j < m; j++) {
+
+                if(arr[i][j] == 0) {
+
+                    // mark row
+                    arr[i][0] = 0;
+
+                    // mark column
+                    if(j != 0) {
+                        arr[0][j] = 0;
+                    }
+                    else {
+                        col0 = 0;
+                    }
+                }
             }
         }
-       } 
 
-       for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-        if(row[i]==1 || col[j]==1){
-            arr[i][j]=0;
+        // Step 2: Traverse from back and fill zeroes
+        for(int i = 1; i < n; i++) {
+
+            for(int j = 1; j < m; j++) {
+
+                if(arr[i][j] != 0) {
+
+                    if(arr[i][0] == 0 || arr[0][j] == 0) {
+
+                        arr[i][j] = 0;
+                    }
+                }
+            }
         }
+
+        // Step 3: First row
+        if(arr[0][0] == 0) {
+
+            for(int j = 0; j < m; j++) {
+
+                arr[0][j] = 0;
+            }
         }
-       }
 
+        // Step 4: First column
+        if(col0 == 0) {
 
+            for(int i = 0; i < n; i++) {
+
+                arr[i][0] = 0;
+            }
+        }
     }
 }
